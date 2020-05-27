5bf3030c61d66907b216e0e52df1ad2a37127dfc
Nepo <braulio.rojas@islas.org.mx>
2020-05-27 12:55:32 -0700

    Quita `projects` pues no corrieron las pruebas

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,4 @@
 module.exports = {
-  projects: [
-    {
-      displayName: "test",
-      preset: "jest-preset-gatsby/typescript",
-      snapshotSerializers: [
-        "jest-serializer-react-helmet",
-        "jest-serializer-json-ld-script",
-      ],
-      collectCoverage: true,
-    },
-    {
-      displayName: "lint:prettier",
-      preset: "jest-runner-prettier",
-      testPathIgnorePatterns: [
-        "/node_modules/",
-        "/coverage/",
-        "/reports/",
-        "/.stryker-tmp/",
-      ],
-    },
-    {
-      displayName: "lint:eslint",
-      runner: "eslint",
-      testMatch: [
-        "<rootDir>/src/**/*.js",
-        "<rootDir>/src/**/*.ts",
-        "<rootDir>/src/**/*.tsx",
-      ],
-    },
-  ],
   transform: {
     '^.+\\.ts?$': 'ts-jest'
   },
