aca82eee34f722715e49746ca90fa50307b7b06f
Nepo <braulio.rojas@islas.org.mx>
2020-05-27 12:52:43 -0700

    Agrega propiedad `projects

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,34 @@
 module.exports = {
+  projects: [
+    {
+      displayName: "test",
+      preset: "jest-preset-gatsby/typescript",
+      snapshotSerializers: [
+        "jest-serializer-react-helmet",
+        "jest-serializer-json-ld-script",
+      ],
+      collectCoverage: true,
+    },
+    {
+      displayName: "lint:prettier",
+      preset: "jest-runner-prettier",
+      testPathIgnorePatterns: [
+        "/node_modules/",
+        "/coverage/",
+        "/reports/",
+        "/.stryker-tmp/",
+      ],
+    },
+    {
+      displayName: "lint:eslint",
+      runner: "eslint",
+      testMatch: [
+        "<rootDir>/src/**/*.js",
+        "<rootDir>/src/**/*.ts",
+        "<rootDir>/src/**/*.tsx",
+      ],
+    },
+  ],
   transform: {
     '^.+\\.ts?$': 'ts-jest'
   },
